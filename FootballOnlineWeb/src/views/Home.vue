31203cd0d75ca070c4a70d3e7c98d600a800cfe1
SSSSeven <321417672@qq.com>
2024-04-08 00:05:07 +0800

    feature:客户端展示轮播图界面效果

--- FootballOnlineWeb/src/views/Home.vue
+++ FootballOnlineWeb/src/views/Home.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="home">
+<div class="home">
     <Header></Header>
-
+    <div id="main">
+      <Banner></Banner>
+    </div>
     <Footer></Footer>
-  </div>
+</div>
 </template>
 
 <script setup>
-// vite中导入非ts.js文件时，必须填写文件后缀
 import Header from "../components/Header.vue"
 import Footer from "../components/Footer.vue"
+import Banner from "../components/Banner.vue"
 
-// 测试CORS的跨域配置是否有问题
-import http from "../utils/http";
-
-// 测试服务端的跨域是否配置成功
-// http.get("/api/home/demo/").then(response=>{
-//   console.log(response.data);
-// })
-
-// 测试服务端的跨域是否配置成功
-http.get("/home/test").then(response=>{
-  console.log(response.data);
-})
 </script>
 
 <style scoped>
