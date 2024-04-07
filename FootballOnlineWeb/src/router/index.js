449da774ef63f3c846e2f02a834c4cc204f0388c
SSSSeven <321417672@qq.com>
2024-04-07 17:01:57 +0800

    创建前端首页和登陆的组件
    搭建好首页和登陆框架

--- FootballOnlineWeb/src/router/index.js
+++ FootballOnlineWeb/src/router/index.js
@@ -2,7 +2,24 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 // 路由列表
 const routes = [
-
+{
+    meta:{
+        title: "球学Online学习平台--首页",
+        keepAlive: true //创建缓存：加速访问
+    },
+    path: '/',         // uri访问地址
+    name: "Home",
+    component: ()=> import("../views/Home.vue")
+  },
+  {
+    meta:{
+        title: "球学Online学习平台--用户登陆",
+        keepAlive: true
+    },
+    path:'/login',      // uri访问地址
+    name: "Login",
+    component: ()=> import("../views/Login.vue")
+  }
 ]
 
 // 路由对象实例化
